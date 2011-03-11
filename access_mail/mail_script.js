d5c0b81d639d62650157b5895faeca9856f21bd6
Joohong Kim <joohong.kim@access-company.com>
2011-03-11 10:30:26 +0900

    minor fix to support label width caused from google content changes

--- access_mail/mail_script.js
+++ access_mail/mail_script.js
@@ -3,8 +3,8 @@ function setLabelColumnWidth(param_width)
 {
 	  var canvas_frame_doc = document.getElementById("canvas_frame");
     var body_tag = canvas_frame_doc.contentDocument.body;
-    var label_div = body_tag.childNodes[1].childNodes[1].childNodes[0].childNodes[1].childNodes[0].childNodes[1];
-    var message_div = body_tag.childNodes[1].childNodes[1].childNodes[0].childNodes[1].childNodes[0].childNodes[2];
+    var label_div = body_tag.childNodes[1].childNodes[1].childNodes[0].childNodes[1].childNodes[0].childNodes[0];
+    var message_div = body_tag.childNodes[1].childNodes[1].childNodes[0].childNodes[1].childNodes[0].childNodes[1];
     
     console.log(label_div.style.width);
     var offset_width;
